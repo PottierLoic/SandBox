5a1c11a57b05e3894010b2c552f871bf0816026e
PottierLoic <loic.pottier@live.fr>
2023-02-13 11:19:17 +0100

    square draw

--- game.py
+++ game.py
@@ -8,16 +8,34 @@ import numpy as np
 # FILE IMPORTS
 from constants import *
 
-
+# contain the main grid and functions needed to draw on it
 class Game:
     def __init__(self) -> None:
-        self.grid = np.zeros((HEIGHT, WIDTH), dtype=int)
+        self.grid = np.zeros((GRID_HEIGHT, GRID_WIDTH), dtype=int)
 
+    #TODO: find the next state/position of all the cells
+    # need to decide if all cells must find their next state themselve (by creating a new object for every cell)
+    # or if everything must be checked here. prob better to do it separately
     def update(self):
         pass
 
+    # draw the selected cell_type on the grid
+    # only replace empty cells
+    #TODO: modify it so it draw a round shape instead of a square
     def add_cell(self, x, y, cell_type):
-        self.grid[y][x] = cell_type
-
-
+        toDraw=[]
+        for dy in range(-int(BRUSH_SIZE)/2, int(BRUSH_SIZE/2)):
+            for dx in range(-int(BRUSH_SIZE)/2, int(BRUSH_SIZE/2)):
+                toDraw.append([x+dx, y+dy])
+        for coords in toDraw:
+            if 0<coords[0]<len(self.grid[0]) and 0<coords[1]<len(self.grid) and self.grid[coords[1]][coords[0]]!=0:
+                self.grid[y][x] = cell_type
     
+    # return a printable string of the grid, used for debug purpose
+    def __str__(self) -> str:
+        s=""
+        for row in self.grid:
+            for value in row:
+                s+=value+" "
+            s+="\n"
+        return s
--- main.py
+++ main.py
@@ -15,23 +15,25 @@ def graphics():
         for x in range(len(g.grid[y])):
             if g.grid[y][x] == 1:
                 canvas.create_rectangle(x * CELL_SIZE, y * CELL_SIZE, x * CELL_SIZE + CELL_SIZE, y * CELL_SIZE + CELL_SIZE, fill="black", tags="cell")
-    window.after(DELAY, graphics)
+   
+def update():
+    graphics()
+    window.after(DELAY, update)
 
 if __name__ == "__main__":
-
     # TKINTER SECTION
     window = Tk()
-    window.title("The sandbox game")
+    window.title("The sandbox")
 
-    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=HEIGHT, width=WIDTH)
+    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GRID_HEIGHT*CELL_SIZE, width=GRID_WIDTH*CELL_SIZE)
     canvas.pack()
 
     window.update()
 
     window.bind("<Button-1>", lambda event: g.add_cell(int(event.x/CELL_SIZE), int(event.y/CELL_SIZE), 1))
 
-    # start
+    # MAIN LOOP
     g = Game()
-    graphics()
+    update()
 
     window.mainloop()
